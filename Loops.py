0bdc2b7a7e02bbc3e4a329e45bf9e1d5453734be
Ayushi Soni <70092571+ayushi-987@users.noreply.github.com>
2024-04-29 16:55:23 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,19 @@
+'''
+Loop:- is used for repeating the instructions.
+
+While Loop:-
+'''
+
+i=int(input("input a number"))
+while i<5:
+    print("Hello World")
+    i=i+1
+
+#WAP to print the multiplication table of a number
+    
+n=1
+while n<=10:
+    prev=n
+    n=n*i
+    print(n)
+    i+=1
