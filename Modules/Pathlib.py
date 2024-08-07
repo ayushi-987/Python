252fa21f3cd7080b3c58d3d37e0d980b26731eaf
Ayushi Soni <70092571+ayushi-987@users.noreply.github.com>
2024-08-07 18:04:06 +0530

    Create Pathlib.py

---
+++ Modules/Pathlib.py
@@ -0,0 +1,8 @@
+import pathlib
+path = pathlib.Path("/etc/hosts")  # It checks the existance of the file and give its content to the file path
+
+dir(path)
+'''
+Output:-
+It give the content of the /etc/hosts methods defined
+'''
